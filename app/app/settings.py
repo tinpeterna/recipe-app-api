ab895535e54c93df3e0ce9db08c55ab93af46e70
tinpeterna <petertinnt@gmail.com>
2020-04-19 14:28:19 +0700

    Configure database in Django

--- app/app/settings.py
+++ app/app/settings.py
@@ -76,6 +76,7 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
